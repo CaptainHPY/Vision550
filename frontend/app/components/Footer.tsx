5b2ab53c8ec9a4851a54e9db9b04625420043770
CaptainHPY <3438371073@qq.com>
2024-12-02 06:39:31 +0800

    docs: add a statement

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <div className="row-start-3 flex flex-col gap-2 items-center justify-center">
+            <span className="flex items-center gap-2">
+                内容由AI生成，请注意甄别。
+            </span>
             <span className="flex items-center gap-2">
                 Designed by <a className="hover:underline hover:underline-offset-4" href="https://github.com/CaptainHPY">CaptainHPY</a>
             </span>
